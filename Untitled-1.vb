a51bf4f92d9a8fe24dbb6b5898b1d32e4090790c
Vicente Roche 1 <Vicente.Roche@GitKraken.com>
2024-04-10 18:39:56 +0200

    vb

---
+++ Untitled-1.vb
@@ -0,0 +1,11 @@
+'------------------------------------------------
+' Date
+' Change
+' Purpose
+'-------------------------------------------------------
+Private Sub lvwItems(ByVal Column As MSComctLib.ColumnHeader)
+
+    SortList lvwItems, ColumnHeader
+    msgbox "hello"
+
+End Sub
